40adf6c82528a07d89983983c3865c83170eaae4
jariheiskanen <jari.heiskanen.koulu@gmail.com>
2025-09-17 14:56:49 +0300

    Add location input and weather chart visualization
    Users can now select the location for weather data via a text input. Added a canvas chart to visualize daily average, minimum, and maximum temperatures. Refactored data parsing and display logic to support dynamic locations and chart rendering.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//compile with tsc .\script.ts
-//export {}
 //page load actions
 window.addEventListener('load', function () {
     updateTimestamp();
@@ -19,7 +17,8 @@ function loadData() {
     var year = document.getElementById("year_select").value;
     var start_date = year + "-" + month + "-01T00:00:00Z";
     var end_date = year + "-" + month + "-" + numDays(year, parseInt(month)) + "T00:00:00Z";
-    var data_url = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=kajaani&parameters=tday,tmin,tmax&starttime=" + start_date + "&endtime=" + end_date;
+    var location = document.getElementById("location").value;
+    var data_url = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + location + "&parameters=tday,tmin,tmax&starttime=" + start_date + "&endtime=" + end_date;
     fetch(data_url)
         .then(response => response.text())
         .then(data => {
@@ -40,13 +39,6 @@ function parseData(data) {
     for (var i = 1; i < split_data.length; i++) {
         var str = split_data[i];
         var time = str.split('<BsWfs:Time>').pop().split('</BsWfs:Time>')[0];
-        //check if day changes for data grouping
-        if (time != loopTime) {
-            //clone object and push object into array here
-            var obj_clone = structuredClone(weather_obj);
-            obj_array.push(obj_clone);
-        }
-        loopTime = time;
         //var pos: string = str.split('<gml:pos>').pop().split('</gml:pos>')[0];
         var parameter = str.split('<BsWfs:ParameterName>').pop().split('</BsWfs:ParameterName>')[0];
         var value = str.split('<BsWfs:ParameterValue>').pop().split('</BsWfs:ParameterValue>')[0];
@@ -59,20 +51,146 @@ function parseData(data) {
         }
         else if (parameter == "tmax") {
             weather_obj.tmax = value;
+            //push object into other array when done with last value of the day
+            var obj_clone = structuredClone(weather_obj);
+            obj_array.push(obj_clone);
         }
         else { }
     }
-    //display data in table
-    var html = "<table><tr><th>Date</th><th>Avg (°C)</th><th>Min (°C)</th><th>Max (°C)</th></tr>";
-    for (var j = 1; j < obj_array.length; j++) {
-        html += "<tr><td>" + obj_array[j].date.split("T")[0] + "</td><td>" + obj_array[j].tday + "</td><td>" + obj_array[j].tmin + "</td><td>" + obj_array[j].tmax + "</td></tr>";
-        document.getElementById("weather_data").innerHTML = html;
+    if (obj_array.length != 0) {
+        //display data in table
+        var html = "<table><tr><th>Date</th><th>Avg (°C)</th><th>Min (°C)</th><th>Max (°C)</th></tr>";
+        for (var j = 0; j < obj_array.length; j++) {
+            html += "<tr><td>" + obj_array[j].date.split("T")[0] + "</td><td>" + obj_array[j].tday + "</td><td>" + obj_array[j].tmin + "</td><td>" + obj_array[j].tmax + "</td></tr>";
+            document.getElementById("weather_data").innerHTML = html;
+        }
+        drawCanvas(obj_array);
     }
+    else {
+        document.getElementById("weather_data").innerHTML = "No data available for the selected time period.";
+        var canvas = document.getElementById("weather_chart");
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "16px Arial";
+        ctx.fillText("No data available for the selected time period", 30, 30);
+    }
+}
+function drawCanvas(data) {
+    var canvas = document.getElementById("weather_chart");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "10px Arial";
+    var chart_range = calcChartRange(data);
+    //get values between highest and lowest in steps of 10
+    var chart_numbers = [];
+    for (var j = chart_range.lowest; j <= chart_range.highest; j++) {
+        chart_numbers.push(j);
+        j = j + 9;
+    }
+    const top_padding = 20;
+    const bottom_padding = 20;
+    const left_padding = 40;
+    const right_padding = 20;
+    const height_padded = canvas.height - top_padding - bottom_padding;
+    //draw horizontal lines for each step
+    for (var k = 0; k < chart_numbers.length; k++) {
+        var pixels_between_steps = height_padded / (chart_numbers.length - 1);
+        var y = canvas.height - bottom_padding - pixels_between_steps * k;
+        ctx.fillText(chart_numbers[k] + " °C", 5, y);
+        ctx.beginPath();
+        ctx.moveTo(left_padding, y);
+        ctx.lineTo(canvas.width - right_padding, y);
+        ctx.strokeStyle = "#cccccc";
+        ctx.stroke();
+    }
+    //draw vertical lines for days
+    var pixels_between_days = (canvas.width - left_padding - right_padding) / (data.length - 1);
+    for (var d = 0; d < data.length; d++) {
+        var x = left_padding + pixels_between_days * d;
+        ctx.beginPath();
+        ctx.moveTo(x, top_padding);
+        ctx.lineTo(x, canvas.height - bottom_padding);
+        ctx.strokeStyle = "#eeeeee";
+        ctx.stroke();
+        ctx.fillText((d + 1).toString(), x - 5, canvas.height - 5);
+    }
+    //draw temperature lines
+    //average
+    ctx.beginPath();
+    for (var a = 0; a < data.length; a++) {
+        if (data[a].tday) {
+            var x = left_padding + pixels_between_days * a;
+            var y = canvas.height - bottom_padding - ((parseFloat(data[a].tday) - chart_range.lowest) / (chart_range.highest - chart_range.lowest)) * height_padded;
+            if (a == 0) {
+                ctx.moveTo(x, y);
+            }
+            else {
+                ctx.lineTo(x, y);
+            }
+            ctx.fillRect(x - 2, y - 2, 4, 4);
+        }
+    }
+    ctx.strokeStyle = "green";
+    ctx.stroke();
+    //minimum
+    ctx.beginPath();
+    for (var b = 0; b < data.length; b++) {
+        if (data[b].tmin) {
+            var x = left_padding + pixels_between_days * b;
+            var y = canvas.height - bottom_padding - ((parseFloat(data[b].tmin) - chart_range.lowest) / (chart_range.highest - chart_range.lowest)) * height_padded;
+            if (b == 0) {
+                ctx.moveTo(x, y);
+            }
+            else {
+                ctx.lineTo(x, y);
+            }
+            ctx.fillRect(x - 2, y - 2, 4, 4);
+        }
+    }
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+    //maximum
+    ctx.beginPath();
+    for (var c = 0; c < data.length; c++) {
+        if (data[c].tmax) {
+            var x = left_padding + pixels_between_days * c;
+            var y = canvas.height - bottom_padding - ((parseFloat(data[c].tmax) - chart_range.lowest) / (chart_range.highest - chart_range.lowest)) * height_padded;
+            if (c == 0) {
+                ctx.moveTo(x, y);
+            }
+            else {
+                ctx.lineTo(x, y);
+            }
+            ctx.fillRect(x - 2, y - 2, 4, 4);
+        }
+    }
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+}
+function calcChartRange(data) {
+    var highest_temp_on_range = 10;
+    var lowest_temp_on_range = 0;
+    for (var j = 1; j < data.length; j++) {
+        if (parseFloat(data[j].tmax) > highest_temp_on_range) {
+            highest_temp_on_range = parseFloat(data[j].tmax);
+        }
+        if (parseFloat(data[j].tmin) < lowest_temp_on_range) {
+            lowest_temp_on_range = parseFloat(data[j].tmin);
+        }
+    }
+    highest_temp_on_range = Math.ceil(highest_temp_on_range / 10) * 10;
+    lowest_temp_on_range = Math.floor(lowest_temp_on_range / 10) * 10;
+    var obj = { highest: highest_temp_on_range, lowest: lowest_temp_on_range };
+    return obj;
 }
 function populateYears() {
     var select = document.getElementById("year_select");
     var currentYear = new Date().getFullYear();
     //check how old data is available from FMI
+    //show error if no data
+    //find closest location with data
+    //tampere 02/2000
+    //kajaani 05/1975
     //https://www.ilmatieteenlaitos.fi/avoin-data-saahavaintojen-vrk-ja-kk-arvot
     //https://www.ilmatieteenlaitos.fi/havaintoasemat
     for (var year = 1950; year <= currentYear; year++) {

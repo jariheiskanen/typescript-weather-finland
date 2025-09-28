80ac5f65b22965a7c93043114727f08f74af03c0
jariheiskanen <jari.heiskanen.koulu@gmail.com>
2025-09-28 16:26:51 +0300

    Update locations data structure and add station UI
    Replaced the municipalities array in locations.js with a detailed array of city objects containing geographic and population data. Updated index.html to add a measurement station selector and info display, added initial dropdown for station-based weather data.

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 //- loading indicator when fetching data
 //- hover for chart points to show values
 //- improve mobile layout
-//- error handling for invalid location input
-//- show message if no data available for selected month/year/location
-//- find closest location with data if no data for requested location
-//- find list of measurement stations from FMI and show those as suggestions
-//- show which measurement station is being used
-//data available since
-//tampere 02/2000
-//kajaani 05/1975
+//- if date older than when station founded is selected, don't include station
+//- better error message
+//- improve layout before data is loaded
+//- improve station selection layout
+//- remember selected station when loading data again from different month
+//features:
 //compare historical data between two locations
+var stations = [];
+var station_distances = [];
 var weather_data = [];
 const top_padding = 20;
 const bottom_padding = 20;
@@ -20,32 +20,78 @@ const right_padding = 20;
 window.addEventListener('load', function () {
     populateYears(); //populate year select options
     selectActiveMonth(); //set current month as selected
-    loadData();
     autoFillLocation();
+    fetchStations(); //fetch regions for location suggestions
 });
 //load data from fmi api
 function loadData() {
-    if (document.getElementById("location").value == "") {
-        //hide canvas etc on load
+    if (document.getElementById("location").value != "") {
+        searchClosestStations();
+    }
+}
+//searches closest stations based on given location within 75km range
+function searchClosestStations() {
+    station_distances = [];
+    //attempt to find closest stations
+    var locationInput = document.getElementById("location");
+    for (var i = 0; i < locations.length; i++) {
+        if (locations[i].city.toLowerCase() == locationInput.value.toLowerCase()) {
+            var coord_lat = parseFloat(locations[i].lat);
+            var coord_lng = parseFloat(locations[i].lng);
+            for (var j = 0; j < stations.length; j++) {
+                var station_coords = stations[j].pos.split(" ");
+                var station_lat = parseFloat(station_coords[0]);
+                var station_lng = parseFloat(station_coords[1]);
+                var distance_to_station = parseFloat(calcDistance(coord_lat, coord_lng, station_lat, station_lng).toFixed(1));
+                //ignore stations if they are over 75km away
+                if (distance_to_station < 75) {
+                    station_distances.push({ fmisid: stations[j].fmisid, name: stations[j].name, region: stations[j].region, pos: stations[j].pos, begin: stations[j].begin, distance: distance_to_station });
+                }
+            }
+            station_distances.sort((a, b) => a.distance - b.distance);
+            //restrict maximum amount of stations to 10
+            if (station_distances.length >= 10) {
+                station_distances = station_distances.slice(0, 10);
+            }
+            //populate station select
+            var select = document.getElementById("station_select");
+            select.innerHTML = "";
+            for (var j = 0; j < station_distances.length; j++) {
+                var option = document.createElement("option");
+                option.value = station_distances[j].fmisid;
+                option.text = station_distances[j].name;
+                select.appendChild(option);
+            }
+            break;
+        }
+    }
+    if (station_distances.length > 0) {
+        connectToAPI();
     }
     else {
-        //fetch amount of days in month
-        const numDays = (y, m) => new Date(y, m, 0).getDate();
-        var month = document.getElementById("month_select").value;
-        var year = document.getElementById("year_select").value;
-        var start_date = year + "-" + month + "-01T00:00:00Z";
-        var end_date = year + "-" + month + "-" + numDays(year, parseInt(month)) + "T00:00:00Z";
-        var location = document.getElementById("location").value;
-        var data_url = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=" + location + "&parameters=tday,tmin,tmax&starttime=" + start_date + "&endtime=" + end_date;
-        fetch(data_url)
-            .then(response => response.text())
-            .then(data => {
-            parseData(data);
-        })
-            .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+        //no stations found within 75km range or incorrect input
+        alert("No results found, check if you have typed correct location");
     }
+}
+//fetch data from FMI API based on input fields
+function connectToAPI() {
+    getStationInfo();
+    //fetch amount of days in month
+    const numDays = (y, m) => new Date(y, m, 0).getDate();
+    var month = document.getElementById("month_select").value;
+    var year = document.getElementById("year_select").value;
+    var start_date = year + "-" + month + "-01T00:00:00Z";
+    var end_date = year + "-" + month + "-" + numDays(year, parseInt(month)) + "T00:00:00Z";
+    var fmisid = document.getElementById("station_select").value;
+    var data_url = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&fmisid=" + fmisid + "&parameters=tday,tmin,tmax&starttime=" + start_date + "&endtime=" + end_date;
+    fetch(data_url)
+        .then(response => response.text())
+        .then(data => {
+        parseData(data);
+    })
+        .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 }
 //parse fetched data
 function parseData(data) {
@@ -78,8 +124,6 @@ function parseData(data) {
     if (weather_data.length != 0) {
         drawTable();
         drawCanvas();
-        document.getElementById("result_view").style.display = "block";
-        //document.getElementById("input_view").style.display = "none";
     }
     else {
         document.getElementById("weather_data").innerHTML = "No data available for the selected time period.";
@@ -90,9 +134,68 @@ function parseData(data) {
         ctx.fillText("No data available for the selected time period", 30, 30);
     }
 }
+function getStationInfo() {
+    var fmisid = document.getElementById("station_select").value;
+    for (var i = 0; i < station_distances.length; i++) {
+        if (station_distances[i].fmisid == fmisid) {
+            var date = station_distances[i].begin.split("T")[0].split("-");
+            var format_date = date[2] + "." + date[1] + "." + date[0];
+            document.getElementById("station_distance").textContent = "Distance: " + station_distances[i].distance + " km";
+            document.getElementById("station_active").textContent = "Active since: " + format_date;
+        }
+    }
+}
+function fetchStations() {
+    var regions_url = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::ef::stations";
+    fetch(regions_url)
+        .then(response => response.text())
+        .then(data => {
+        parseStations(data);
+    })
+        .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+function parseStations(data) {
+    var split_data = data.split("<wfs:member>");
+    stations = [];
+    for (var i = 1; i < split_data.length; i++) {
+        var str = split_data[i];
+        if (str.search("Automaattinen sääasema") != -1) //only include automatic weather stations
+         {
+            var fmisid = str.split('codeSpace="http://xml.fmi.fi/namespace/stationcode/fmisid">').pop().split('</gml:identifier>')[0];
+            var name = str.split('codeSpace="http://xml.fmi.fi/namespace/locationcode/name">').pop().split('</gml:name>')[0];
+            var region = str.split('codeSpace="http://xml.fmi.fi/namespace/location/region">').pop().split('</gml:name>')[0];
+            var pos = str.split('<gml:pos>').pop().split('</gml:pos>')[0];
+            var begin = str.split('<gml:beginPosition>').pop().split('</gml:beginPosition>')[0];
+            stations.push({ fmisid: fmisid, name: name, region: region, pos: pos, begin: begin });
+        }
+    }
+}
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371; // km
+    var dLat = toRad(lat2 - lat1);
+    var dLon = toRad(lon2 - lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d;
+}
+// Converts numeric degrees to radians
+function toRad(value) {
+    return value * Math.PI / 180;
+}
 //auto fill location input with suggestions
 function autoFillLocation() {
-    const municipalities = locations.municipalities;
+    //const municipalities = locations.municipalities;
+    var municipalities = [];
+    for (var i = 0; i < locations.length; i++) {
+        municipalities.push(locations[i].city);
+    }
     var locationInput = document.getElementById("location");
     locationInput.addEventListener("input", function () {
         var inputValue = locationInput.value.toLowerCase();
@@ -170,6 +273,12 @@ function drawCanvas() {
     }
     //draw temperature lines
     drawLines(ctx, chart_range, height_padded, pixels_between_days);
+    //scroll to chart
+    window.scrollTo({
+        top: 1000,
+        left: 0,
+        behavior: "smooth",
+    });
 }
 //draw temperature lines and points
 function drawLines(ctx, chart_range, height_padded, pixels_between_days) {
@@ -231,7 +340,7 @@ function calcChartRange() {
 function populateYears() {
     var select = document.getElementById("year_select");
     var currentYear = new Date().getFullYear();
-    for (var year = 1950; year <= currentYear; year++) {
+    for (var year = 1900; year <= currentYear; year++) {
         var option = document.createElement("option");
         option.value = year.toString();
         option.text = year.toString();
@@ -248,7 +357,9 @@ function selectActiveMonth() {
 function legendChanged() {
     drawCanvas();
 }
-/* info
+/*
+
+info
 https://en.ilmatieteenlaitos.fi/open-data-manual-fmi-wfs-services
 https://en.ilmatieteenlaitos.fi/open-data-manual-time-series-data
 https://www.ilmatieteenlaitos.fi/tallennetut-kyselyt
@@ -258,18 +369,10 @@ https://en.ilmatieteenlaitos.fi/open-data-manual-wfs-examples-and-guidelines
 https://www.ilmatieteenlaitos.fi/avoin-data-saahavaintojen-vrk-ja-kk-arvot
 https://www.ilmatieteenlaitos.fi/havaintoasemat
 
-fmi::observations::weather::daily::simple
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=describeStoredQueries&storedquery_id=fmi::observations::weather::daily::simple
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=kajaani
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=kajaani&parameters=tday,tmin,tmax
-
-lämpötila arvot kajaaniin heinäkuun ajalta
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=kajaani&parameters=tday,tmin,tmax&starttime=2012-07-01T00:00:00Z&endtime=2012-07-31T00:00:00Z
-3 different measurement stations
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::simple&place=kajaani&maxlocations=3&parameters=tday,tmin,tmax&starttime=2012-07-01T00:00:00Z&endtime=2012-07-31T00:00:00Z
 list of stations
 https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::ef::stations
 
-fmi::observations::weather::daily::timevaluepair
+https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::daily::timevaluepair&place=kajaani&maxlocations=5&parameters=tday,tmin,tmax&starttime=1960-07-01T00:00:00Z&endtime=1960-07-31T00:00:00Z
+
 */ 
 //# sourceMappingURL=script.js.map
